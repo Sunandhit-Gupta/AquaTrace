1a387dca7e7356d44402aad40669392ab2f1fc0d
Gaurav Upadhyay <gauravupadhyay.work@gmail.com>
2023-09-26 23:26:26 +0530

    asdv

--- aqua_trace/lib/textfield.dart
+++ aqua_trace/lib/textfield.dart
@@ -18,33 +18,72 @@ class _TextIn extends State<TextWidget> {
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
-        Autocomplete<String>(
-          optionsBuilder: (TextEditingValue textinput) {
-            temp = textinput.text;
-            if (textinput.text == '') {
-              return const Iterable<String>.empty();
-            }
-            return listItems.where((String item) {
-              return item.contains(textinput.text.toLowerCase());
-            });
-          },
-        ),
-        // Text field for input
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 300,
+                height: 50,
+                child: Center(
+                  child: Autocomplete<String>(
+                    optionsBuilder: (TextEditingValue textinput) {
+                      temp = textinput.text;
+                      if (textinput.text == '') {
+                        return const Iterable<String>.empty();
+                      }
+                      return listItems.where((String item) {
+                        return item.contains(textinput.text.toLowerCase());
+                      });
+                    },
+                    fieldViewBuilder:
+                        (context, controller, focusNode, onEditingComplete) {
+                      return TextField(
+                        controller: controller,
+                        focusNode: focusNode,
+                        onEditingComplete: onEditingComplete,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10))),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              // Text field for input
 
-        // Button to add to the list
-        TextButton(
-          onPressed: () {
-            String newItem = temp.trim();
-            if (newItem.isNotEmpty) {
-              setState(() {
-                items.add(newItem);
-                textController.clear();
-              });
-            }
-          },
-          child: Text('Add'),
-          style: TextButton.styleFrom(
-            textStyle: const TextStyle(fontSize: 20),
+              // Button to add to the list
+
+              Center(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 50,
+                    width: 70,
+                    child: TextButton(
+                      onPressed: () {
+                        String newItem = temp.trim();
+                        if (newItem.isNotEmpty) {
+                          setState(() {
+                            items.add(newItem);
+                            textController.clear();
+                          });
+                        }
+                      },
+                      child: Text('+'),
+                      style: TextButton.styleFrom(
+                        textStyle: const TextStyle(fontSize: 20),
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.blue,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
 
